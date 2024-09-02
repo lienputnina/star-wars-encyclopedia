c799c43a85cf84321eae2ce800136c4a5595bcf3
lienputnina <liene.putnina20@gmail.com>
2024-09-02 17:38:02 +0300

    refactor: moved removed CharacterDetails component

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -1,35 +1,103 @@
-import { NextPage } from 'next';
+import { gql } from '@apollo/client';
+import { serverClient } from '@/server/apollo-client';
+
 import ErrorPage from '@/components/ErrorPage';
-import { CharacterDetails } from '@/components/CharacterDetails';
-import { getCharacterData } from '@/server/getCharacterData';
 import { CharacterNotFound } from '@/components/CharacterNotFound';
 
-interface CharacterPage {
-  params: {
-    id: string;
+const GET_CHARACTER = gql`
+  query GetCharacter($id: ID!) {
+    person(id: $id) {
+      name
+      birthYear
+      species {
+        name
+      }
+      homeworld {
+        name
+        climates
+        gravity
+        terrains
+      }
+      filmConnection {
+        films {
+          title
+        }
+      }
+    }
+  }
+`;
+
+export interface Character {
+  person: {
+    name: string;
+    birthYear: string;
+    species: { name: string | null };
+    homeworld: {
+      name: string;
+      climates: string;
+      gravity: string;
+      terrains: string[];
+    };
+    filmConnection: {
+      films: [{ title: string }];
+    };
   };
+  id: string;
 }
 
-const CharacterPage = async ({ params }: CharacterPage) => {
+const CharacterPage = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
 
+  let character = null;
+  let error = null;
+
   try {
-    const character = await getCharacterData(id);
+    const { data: characterData } = await serverClient.query<Character>({
+      query: GET_CHARACTER,
+      variables: { id: decodeURIComponent(id) },
+    });
 
-    if (!character) {
-      return <CharacterNotFound />;
+    if (characterData?.person) {
+      character = characterData.person;
+    } else {
+      console.error('Character data is missing');
     }
-
-    return (
-      <div className="p-10">
-        <h1 className="text-center my-4 text-xl">{character.name}</h1>
-        <CharacterDetails character={character} />
-      </div>
-    );
   } catch (error: any) {
-    console.error(error.message);
-    return <ErrorPage message={error.message} />;
+    if (error.networkError) {
+      error =
+        'Network error: Unable to reach the server. Please check your internet connection and try again.';
+    } else if (error.graphQLErrors?.length > 0) {
+      error =
+        'Error fetching data: The requested character data could not be retrieved. Please try again later.';
+    } else {
+      error =
+        'Unexpected error: Something went wrong while fetching the character data. Please refresh the page or try again later.';
+    }
   }
+  if (error) {
+    return <ErrorPage message={error} />;
+  }
+  if (!character) {
+    return <CharacterNotFound />;
+  }
+
+  return (
+    <div className="p-10">
+      <h1 className="text-lg text-center mb-4">{character.name}</h1>
+      <p className=" p-0.5">Birth Year: {character.birthYear}</p>
+      <p className=" p-0.5">Species: {character.species?.name || 'unknown'}</p>
+      <div className="homeworld flex-col">
+        <p className="p-0.5 justify-self-start">
+          Homeworld:{' '}
+          {`${character.homeworld?.name}, ${character.homeworld?.climates || 'unknown'} climate, ${character.homeworld?.gravity || 'unknown'} gravity, ${character.homeworld?.terrains?.join(', ') || 'unknown'} terrain`}
+        </p>
+      </div>
+      <p className=" p-0.5">
+        Films:{' '}
+        {character.filmConnection?.films.map((film) => film.title).join(', ')}
+      </p>
+    </div>
+  );
 };
 
 export default CharacterPage;
